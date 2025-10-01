aabd357cdc3d71889cd7a10eb57e7aa43f836bff
b9aurav <hey9aurav@gmail.com>
2025-10-01 17:01:45 +0530

    test: add end-to-end distribution testing

--- src/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-describe('APE Core', () => {
-  it('should pass basic test', () => {
-    expect(1 + 1).toBe(2);
-  });
-  
-  it('should have Node.js environment', () => {
-    expect(process.env.NODE_ENV).toBeDefined();
-  });
-});
